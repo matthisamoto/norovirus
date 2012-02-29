4ebba6c397daa5e251cfc08f759bebc83146970c
Matt Hisamoto <mwhisamoto@gmail.com>
2012-02-29 15:39:38 -0500

    validation updation

--- public/javascripts/norovirus.js
+++ public/javascripts/norovirus.js
@@ -25,8 +25,9 @@ var List = {
 	  var name  = $.trim($('.user_name').val())
 	  var loc   = $.trim($('.user_location').val())
 	  var story = $.trim($('.user_story').val())
-	  if( name.length > 0 && loc.length > 0 && story.length > 0 && story.toLowerCase() != "SHARE YOUR STORY\n( IT'S OKAY TO LEAVE STUFF OUT )".toLowerCase() && name.toLowerCase() != "NAME ( FIRST, LAST INITIAL )".toLowerCase() && loc.toLowerCase() != "LOCATION ( CITY, STATE )".toLowerCase() ) {
-		
+	  if( name.length > 0 && loc.length > 0 && story.length > 0 && name.toLowerCase() != "NAME ( FIRST, LAST INITIAL )".toLowerCase()) {
+		if(story.toLowerCase() == "SHARE YOUR STORY\n( IT'S OKAY TO LEAVE STUFF OUT )".toLowerCase()) $('.user_story').val("");
+		if(loc.toLowerCase() == "LOCATION ( CITY, STATE )".toLowerCase()) $('.user_location').val("");
 	  } else {
 		alert("Please fill out all fields.")
 		return false;
